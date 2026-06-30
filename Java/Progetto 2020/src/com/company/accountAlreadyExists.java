47121f1f85738d892b15e82b7195346b7d2a8a10
Matteo Di Giorgio <69473180+MatteoDiGiorgioCodes@users.noreply.github.com>
2026-06-30 11:34:04 +0200

    Delete Java/Progetto 2020 directory

--- Java/Progetto 2020/src/com/company/accountAlreadyExists.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class accountAlreadyExists extends Exception
-{
-    public accountAlreadyExists(String s) { super(s); }
-}
--- Java/Progetto 2020/src/com/company/postNotFound.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public class postNotFound extends Exception
-{
-    public postNotFound(String s) { super(s); }
-}
